7bef3f70cf314029c915023af14f688f53bb3f77
Ivan Koptev <koptev.ivan.97@mail.ru>
2019-04-09 19:21:57 +0300

    Лабораторна iterator

--- iterator/include/iterator.hpp
+++ iterator/include/iterator.hpp
@@ -16,9 +16,73 @@
 
 //{ image iterator
 template<class Iterator>
-class image_iterator: public boost::iterator_adaptor<...>
+class image_iterator: public boost::iterator_adaptor<image_iterator<Iterator>, Iterator>
 {
+public:
+    using diff_type = typename image_iterator::difference_type;
 
+    image_iterator(Iterator iter, size_t width, size_t stride) :
+            image_iterator::iterator_adaptor_(iter),
+            width_(width),
+            stride_(stride)
+    {}
+
+    void advance(diff_type n)
+    {
+        size_t rest = (n > 0) ? width_ - index_ % stride_  - 1: index_ % stride_;
+        size_t stride_count = 0;
+
+        if (abs(n) > (int)rest)
+        {
+            stride_count = ceil((double)(abs(n) - rest) / width_);
+        }
+        int res = n/abs(n) * (stride_count * (stride_ - width_) + abs(n));
+        this->base_reference() += res;
+        index_ += res;
+    }
+
+    diff_type distance_to( image_iterator const &x ) const
+    {
+        auto n = x.base() - this->base();
+        auto rest = (n > 0) ? index_ : x.index_ % stride_;
+        auto s = abs(n) + rest;
+        auto res = abs(n)/n * (s/(int)stride_ * (int)width_ + s%(int)stride_  - rest);
+
+        return res;
+    }
+
+    void increment()
+    {
+        if (index_ % stride_ == 0)
+        {
+            this->base_reference() += stride_ - width_ + 1;
+            index_ += stride_ - width_ + 1;
+        }
+        else
+        {
+            this->base_reference()++;
+            index_++;
+        }
+    }
+
+    void decrement()
+    {
+        if (width_- index_ % (int)stride_ == 0)
+        {
+            this->base_reference() -= width_- index_ % (int)stride_ + 1;
+            index_ += width_- index_ % (int)stride_ + 1;
+        }
+        else
+        {
+            this->base_reference()--;
+            index_--;
+        }
+    }
+
+private:
+    size_t width_;
+    size_t stride_;
+    size_t index_ = 0;
 };
 //}
 
@@ -26,6 +90,7 @@ template<class Container = std::vector<uint8_t>>
 class image
 {
 public:
+    // stride > width
     image(size_t width, size_t height, size_t stride):
         data(stride * height),
         width_(width),
