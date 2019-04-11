28a0f3cf46cf744196c8e9905b6e0b5df01c2744
Ivan Koptev <koptev.ivan.97@mail.ru>
2019-04-12 00:50:28 +0300

    Часть json

--- parser/include/variant_decorator.hpp
+++ parser/include/variant_decorator.hpp
@@ -16,18 +16,29 @@ namespace x3 = boost::spirit::x3;
 //? How many constructors did I inherit?
 //? Do I need my own constructor & ``operator=``? Why?
 //{
-template<???>
-struct variant_decorator: public x3::variant<???>
+template<class... Ts>
+struct variant_decorator: public x3::variant<Ts...>
 {
-    using ???operator=;
-    using ...
+    using x3::variant<Ts...>::operator=;
+    using x3::variant<Ts...>::variant;
+
+    variant_decorator()=default;
+    variant_decorator(variant_decorator const&)=default;
+    variant_decorator& operator=(variant_decorator const&)=default;
 
     //? Do I need a ``const``-version?
     template<class T>
     T& as()
     {
-        return ...
+        return boost::get<T>(*this);
     }
+
+    template<class T>
+    const T& as() const
+    {
+        return boost::get<T>(*this);
+    }
+};
 //}
 
 #endif // __VARIANT_DECORATOR_HPP__
